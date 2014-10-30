efdd304125df54eb275343ffb2b8f53467f76466
zengdh <zengdh-fnst@cn.fujitsu.com>
2014-10-30 16:47:48 +0800

    trial of remain cases
    1. function 'cleanup_dest' and 'thread_func_live_migration'
       are copied from 'virsh_migrate_setmaxdowntime.py', with
       a bit modification.

--- libvirt/tests/src/cpu_features.py
+++ libvirt/tests/src/cpu_features.py
@@ -5,6 +5,10 @@ import time
 
 from autotest.client import os_dep, utils
 from autotest.client.shared import error
+from virttest.utils_test import libvirt as utlv
+
+global is_mig_success
+is_mig_success = False
 
 def _nbench_help():
     """
@@ -32,12 +36,39 @@ def find_index_values(filename):
 
         integer_index = match_integer.group(0)
         integer_index = integer_index.split(":")[1].lstrip()
-        integer_index = float(integer_index)
         floating_index = match_floating.group(0)
         floating_index = floating_index.split(":")[1].lstrip()
-        floating_index = float(floating_index)
 
-        return (integer_index, floating_index)
+        return (float(integer_index), float(floating_index))
+
+def cleanup_dest(vm, src_uri, dest_uri):
+    """
+    Cleanup migrated guest on destination.
+    Then reset connect uri to src_uri
+    """
+    vm.connect_uri = dest_uri
+    if vm.exists():
+        if vm.is_persistent():
+            vm.undefine()
+        if vm.is_alive():
+            vm.destroy()
+    # Set connect uri back to local uri
+    vm.connect_uri = src_uri
+
+def thread_func_live_migration(vm, dest_uri, dargs):
+    """
+    Thread for virsh migrate command.
+    """
+    # Migrate the domain.
+    debug = dargs.get("debug", "False")
+    ignore_status = dargs.get("ignore_status", "False")
+    options = "--live"
+    extra = dargs.get("extra")
+    global is_mig_success
+    result = vm.migrate(dest_uri, options, extra, ignore_status, debug)
+    if result.exit_status:
+        return
+    is_mig_success = True
 
 def run(test, params, env):
     """
@@ -56,16 +87,14 @@ def run(test, params, env):
         raise error.TestNAError("No nbench found in host").
 
     dir_of_nbench = params.get('dir_of_nbench')
+    mig_thread_timeout = params.get('mig_thread_timeout', 180)
     #vms
     vm_name_hv_default = params.get("hv_default")
     vm_name_nearest_host = params.get("nearest_host")
     vm_name_copy_host = params.get("copy_host")
-    if vm_name_copy_host.count('ENTER') or vm_name_nearest_host.count('ENTER')
-       or vm_name_hv_default.count('ENTER'):
-         raise error.TestNAError('Please enter host names in .cfg files') 
-    vm_hv_default = env.get_vm(vm_name_hv_default)
-    vm_nearest_host = env.get_vm(vm_name_nearest_host)
-    vm_copy_host = env.get_vm(vm_name_copy_host)
+    if vm_name_copy_host.count('ENTER') or vm_name_nearest_host.count('ENTER') or
+       vm_name_hv_default.count('ENTER'):
+        raise error.TestNAError('Please enter host names in .cfg files') 
 
     #test branches
     is_hv_default == ("yes" == params.get("cpu_features_hypervisor_default"));
@@ -79,36 +108,72 @@ def run(test, params, env):
     result_on_guest = "/root/result"
 
     try:
-        if is_hv_default and is_nearest_host:
+        if is_hv_default:
             guest_command = "cd %s; ./nbench > %s" % (dir_of_nbench, result_on_guest)
 
+            vm_hv_default = env.get_vm(vm_name_hv_default)
             session_hv_default = vm_hv_default.wait_for_login()
             session_hv_default.cmd_status_output(guest_command)
             session_hv_default.close()
             vm.copy_files_from(result_on_guest, hv_default_result_file)
             ii_hv_default, fi_hv_default = find_index_values(hv_default_result_file)
 
-            session_nearest_host = vm_nearest_host.wait_for_login()
-            session_nearest_host.cmd_status_output(guest_command)
-            session_nearest_host.close()
-            vm.copy_files_from(result_on_guest, session_nearest_host)
-            ii_nearest_host, fi_nearest_host = find_index_values(hv_default_result_file)
+            if is_nearest_host:
+                vm_nearest_host = env.get_vm(vm_name_nearest_host)
+                session_nearest_host = vm_nearest_host.wait_for_login()
+                session_nearest_host.cmd_status_output(guest_command)
+                session_nearest_host.close()
+                vm.copy_files_from(result_on_guest, nearest_host_result_file)
+                ii_nearest_host, fi_nearest_host = find_index_values(nearest_host_result_file)
 
-            hv_is_slower = ii_hv_default < ii_nearest_host and 
-                           fi_hv_default < fi_nearest_host
-            if not hv_is_slower:
-                raise error.TestFail("hv_default's performance is better than nearest_host."
-                                     "hv_default's result is (%s,%s), nearest_host's result is (%s,%s)."
-                                     % (ii_hv_default, fi_hv_default, ii_nearest_host, fi_nearest_host))
+                hv_is_slower = ii_hv_default < ii_nearest_host and 
+                               fi_hv_default < fi_nearest_host
+                if not hv_is_slower:
+                    raise error.TestFail("hv_default's performance is better than nearest_host."
+                                         "hv_default's result is (%s,%s), nearest_host's result is (%s,%s)."
+                                         % (ii_hv_default, fi_hv_default, ii_nearest_host, fi_nearest_host))
+            elif is_copy_host:
+                vm_copy_host = env.get_vm(vm_name_copy_host)
+                session_copy_cpu = vm_copy_cpu.wait_for_login()
+                session_copy_cpu.cmd_status_output(guest_command)
+                session_copy_cpu.close()
+                vm.copy_files_from(result_on_guest, copy_cpu_result_file)
+                ii_copy_cpu, fi_copy_cpu = find_index_values(copy_cpu_result_file)
 
-        if is_hv_default and is_copy_host:
-            #similar to the previous case, replace vm_nearest_cpu with vm_copy_cpu
+                hv_is_slower = ii_hv_default < ii_copy_cpu and 
+                               fi_hv_default < fi_copy_cpu
+                if not hv_is_slower:
+                    raise error.TestFail("hv_default's performance is better than copy_cpu."
+                                         "hv_default's result is (%s,%s), copy_cpu's result is (%s,%s)."
+                                         % (ii_hv_default, fi_hv_default, ii_copy_cpu, fi_copy_cpu))
 
-        if (not is_hv_default) and is_nearest_host:
+        if not is_hv_default:
             #need migration
+            src_uri = params.get("migrate_src_uri", "qemu+ssh://EXAMPLE/system")
+            dest_uri = params.get("migrate_dest_uri", "qemu+ssh://EXAMPLE/system")
+            if is_nearest_host:
+                vm = env.get_vm(vm_name_nearest_host)   //TODO
+            elif is_copy_host:
+                vm = env.get_vm(vm_name_copy_host)   //TODO
+            vm.wait_for_login()
+            migrate_dargs = {'debug': True, 'ignore_status': True}
 
-        if (not is_hv_default) and is_copy_host:
-            #need migration
+            try:
+                mig_thread = threading.Thread(target=thread_func_live_migration,
+                                              args=(vm, dest_uri,
+                                                    migrate_dargs)))
+                mig_thread.start()
+                thread.join(mig_thread_timeout)
+
+            finally:
+                cleanup_dest(vm, src_uri, dest_uri)
+
+            if is_nearest_host:
+                if not is_mig_success:
+                    raise error.TestFail("Migration failed.")
+            elif is_copy_host:
+                if is_mig_success:
+                    raise error.testFail("Migration should fail, but successed")
 
     finally:
         if os.path.exists(hv_default_result_file):
